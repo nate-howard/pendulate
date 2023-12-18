3d732f87b7a4bcb440de3a7b966e6540c0ec6a21
Nate Howard <nathanhaashoward@gmail.com>
2023-12-18 15:12:25 -0700

    Controller workinggs!

--- pendulate.cc
+++ pendulate.cc
@@ -24,19 +24,18 @@
 
 
 // bdd output files
-std::string bddDir = "./bdd/";
+std::string bddDir = "./bdd/stay_";
 std::string stateSpaceFile = bddDir + "pendulate_ss.bdd";
-std::string obstaclesFile = bddDir + "pendulate_obst.bdd";
 std::string targetFile = bddDir + "pendulate_target.bdd";
 std::string controllerFile = bddDir + "pendulate_controller.bdd";
 std::string transitionRelationFile = bddDir + "pendulate_transition_relation.bdd";
 
 /* state space dim */
-#define sDIM 3
+#define sDIM 2
 #define iDIM 1
 
 /* data types for the ode solver */
-typedef std::array<double,3> state_type;
+typedef std::array<double,2> state_type;
 typedef std::array<double,1> input_type;
 
 /* sampling time */
@@ -49,10 +48,10 @@ OdeSolver ode_solver(sDIM,nint,tau);
 const double A = 0.1; // Amplitude of vertical driving motion
 const double L = 1; // Length of the pendulum
 const double g = 9.81; // Acceleration of gravity
-const double b = 0.05; // Linear drag term
+// const double b = 0.05; // Linear drag term
 
 const double alpha = g / L;
-const double beta = A / L;
+const double beta = A * A / (4 * L * L);
 
 /* we integrate the pendulate ode by tau sec (the result is stored in x)  */
 auto  pendulate_post = [](state_type &x, input_type &u) -> void {
@@ -60,127 +59,39 @@ auto  pendulate_post = [](state_type &x, input_type &u) -> void {
   /* the ode describing the pendulate */
   auto rhs =[](state_type& dxdt,  const state_type &x, input_type &u) -> void {
       dxdt[0] = x[1];
-      dxdt[1] = (beta*u[0]*u[0]*std::cos(x[2]) - alpha)*std::sin(x[0]) - b*x[1];
-      dxdt[2] = u[0];
+      dxdt[1] = -alpha*std::sin(x[0]) - beta*u[0]*u[0]*std::sin(2*x[0]);
   };
   ode_solver(rhs,x,u);
   
   x[0] = x[0] - 2*M_PI*std::floor(x[0] / (2*M_PI)); // Wrap theta around [0, 2pi]
-  x[2] = x[2] - 2*M_PI*std::floor(x[2] / (2*M_PI)); // Wrap phase around [0, 2pi]
 };
 
 /* computation of the growth bound (the result is stored in r)  */
-auto radius_post = [](state_type &r, input_type &u) -> void {
-    r[0] = r[0]+r[2]*std::abs(u[0])*0.3;
-    r[1] = r[1]+r[2]*std::abs(u[0])*0.3;
+auto radius_post = [](state_type &r, state_type &x, input_type &u) -> void {
+  bool print = ((double) std::rand() / (RAND_MAX)) < (1/500000.0);
+  if(print) std::cout << "r0=[" << r[0] << "," << r[1] << "], x=[" << x[0] << "," << x[1] << "], u = " << u[0] << std::endl;
+  // double bw = beta * u[0];
+  double new_r1 = r[1] + r[0]*abs(alpha*std::cos(x[0]) + 2*beta*u[0]*u[0]*std::cos(2*x[0]))*tau;
+  r[0] = r[0] + new_r1*tau;
+  r[1] = new_r1;
+  if(print) std::cout << "r1 = [" << r[0] << ", " << r[1] << "]" << std::endl;
 };
 
 
-/* forward declaration of the functions to setup the state space 
+/* functions to setup the state space 
  * and input space of the pendulate example */
-scots::SymbolicSet pendulateCreateStateSpace(Cudd &mgr);
-scots::SymbolicSet pendulateCreateInputSpace(Cudd &mgr);
-
-int main() {
-  /* to measure time */
-  TicToc tt;
-  /* there is one unique manager to organize the bdd variables */
-  Cudd mgr;
-
-  /****************************************************************************/
-  /* construct SymbolicSet for the state space */
-  /****************************************************************************/
-  scots::SymbolicSet ss=pendulateCreateStateSpace(mgr);
-  ss.writeToFile(stateSpaceFile.c_str());
-  /* write SymbolicSet of obstacles to pendulate_obst.bdd */
-  // ss.complement();
-  // ss.writeToFile(obstaclesFile.c_str());
-  // ss.complement();
-  std::cout << "Unfiorm grid details:" << std::endl;
-  ss.printInfo(1);
-
-  /****************************************************************************/
-  /* the target set */
-  /****************************************************************************/
-  /* first make a copy of the state space so that we obtain the grid
-   * information in the new symbolic set */
-  scots::SymbolicSet ts = ss;
-  /* define the target set as a symbolic set */
-  double H[4*sDIM]={-1, 0, 0, // This matrix lets us simply define bounds on x[0] and x[1]
-                    1, 0, 0,
-                    0,-1, 0,
-                    0, 1, 0};
-  /* compute inner approximation of P={ x | H x<= h1 }  */
-  double h[4] = {-3,3.3, 0.1,0.1}; // {-lb0, ub0, -lb1, up1}
-  ts.addPolytope(4,H,h, scots::INNER);
-  ts.writeToFile(targetFile.c_str());
-
-
-  /****************************************************************************/
-  /* construct SymbolicSet for the input space */
-  /****************************************************************************/
-  scots::SymbolicSet is=pendulateCreateInputSpace(mgr);
-  std::cout << std::endl << "Input space details:" << std::endl;
-  is.printInfo(1);
-
-  /****************************************************************************/
-  /* setup class for symbolic model computation */
-  /****************************************************************************/
-  /* first create SymbolicSet of post variables 
-   * by copying the SymbolicSet of the state space and assigning new BDD IDs */
-  scots::SymbolicSet sspost(ss,1);
-  /* instantiate the SymbolicModel */
-  scots::SymbolicModelGrowthBound<state_type,input_type> abstraction(&ss, &is, &sspost);
-  /* compute the transition relation */
-  tt.tic();
-  abstraction.computeTransitionRelation(pendulate_post, radius_post);
-  std::cout << std::endl;
-  tt.toc();
-  /* get the number of elements in the transition relation */
-  std::cout << std::endl << "Number of elements in the transition relation: " << abstraction.getSize() << std::endl;
-
-
-  /****************************************************************************/
-  /* we continue with the controller synthesis */
-  /****************************************************************************/
-  int verbose=1;
-  /* we setup a fixed point object to compute reachabilty controller */
-  scots::FixedPoint fp(&abstraction);
-  /* the fixed point algorithm operates on the BDD directly */
-  BDD T = ts.getSymbolicSet();
-  tt.tic();
-  BDD C = fp.reach(T,verbose);
-  tt.toc();
-
-  /****************************************************************************/
-  /* last we store the controller as a SymbolicSet 
-   * the underlying uniform grid is given by the Cartesian product of 
-   * the uniform gird of the space and uniform gird of the input space */
-  /****************************************************************************/
-  scots::SymbolicSet controller(ss,is);
-  controller.setSymbolicSet(C);
-  controller.writeToFile(controllerFile.c_str());
-
-  scots::SymbolicSet tr = abstraction.getTransitionRelation();
-  tr.writeToFile(transitionRelationFile.c_str());
-  
-  return 0;
-}
-
 scots::SymbolicSet pendulateCreateStateSpace(Cudd &mgr) {
 
-  const double maxOmega = 11;
+  const double maxOmega = 8;
 
   /* setup the workspace of the synthesis problem and the uniform grid */
   /* lower bounds of the hyper rectangle */
-  double lb[sDIM]={0,-maxOmega, 0};
+  double lb[sDIM]={0,-maxOmega};
   /* upper bounds of the hyper rectangle */
-  double ub[sDIM]={2*M_PI,maxOmega, 2*M_PI}; 
+  double ub[sDIM]={2*M_PI,maxOmega}; 
   /* grid node distance diameter */
-  double eta[sDIM]={.1, .5, .2};
-  // double eta[sDIM]={1, 5, 1};
-
-
+  double eta[sDIM]={.05, .05};
+  // double eta[sDIM]={.2, .5, .5};
 
   /* eta is added to the bound so as to ensure that the whole
    * [0,10]x[0,10]x[-pi-eta,pi+eta] is covered by the cells */
@@ -209,3 +120,156 @@ scots::SymbolicSet pendulateCreateInputSpace(Cudd &mgr) {
   return is;
 }
 
+int main2() {
+  state_type x = {2.5, 0};
+  input_type u = {50};
+
+  int iter = 300;
+  for(int i=0; i<iter; i++) {
+    pendulate_post(x, u);
+    std::cout << "[" << x[0] << ", " << x[1] << ", "<< x[2] << "]" << std::endl;
+  }
+
+  return 0;
+}
+
+int main() {
+  /* to measure time */
+  TicToc tt;
+  /* there is one unique manager to organize the bdd variables */
+  Cudd mgr;
+
+  /****************************************************************************/
+  /* construct SymbolicSet for the state space */
+  /****************************************************************************/
+  scots::SymbolicSet ss=pendulateCreateStateSpace(mgr);
+  ss.writeToFile(stateSpaceFile.c_str());
+
+  std::cout << "Unfiorm grid details:" << std::endl;
+  ss.printInfo(1);
+
+  /****************************************************************************/
+  /* the target set */
+  /****************************************************************************/
+  /* first make a copy of the state space so that we obtain the grid
+   * information in the new symbolic set */
+
+  scots::SymbolicSet ts = ss;
+  /* define the target set as a symbolic set */
+  double H[4*sDIM]={-1, 0, // This matrix lets us simply define bounds on x[0] and x[1]
+                    1, 0,
+                    0,-1,
+                    0, 1};
+  /* compute inner approximation of P={ x | H x<= h1 }  */
+  double h[4] = {-2.5,2.9, 0.2,0.2}; // {-lb0, ub0, -lb1, up1}
+  ts.addPolytope(4,H,h, scots::INNER);
+  ts.writeToFile(targetFile.c_str());
+  std::cout << std::endl << "Target set info:" << std::endl;
+  ts.printInfo(1);
+
+
+  /****************************************************************************/
+  /* construct SymbolicSet for the input space */
+  /****************************************************************************/
+  scots::SymbolicSet is=pendulateCreateInputSpace(mgr);
+  std::cout << std::endl << "Input space details:" << std::endl;
+  is.printInfo(1);
+
+  /****************************************************************************/
+  /* setup class for symbolic model computation */
+  /****************************************************************************/
+  /* first create SymbolicSet of post variables 
+   * by copying the SymbolicSet of the state space and assigning new BDD IDs */
+  scots::SymbolicSet sspost(ss,1);
+  /* instantiate the SymbolicModel */
+  scots::SymbolicModelGrowthBound<state_type,input_type> abstraction(&ss, &is, &sspost);
+  /* compute the transition relation */
+  tt.tic();
+  abstraction.computeTransitionRelation(pendulate_post, radius_post);
+  std::cout << std::endl;
+  tt.toc();
+  /* get the number of elements in the transition relation */
+  std::cout << std::endl << "Number of elements in the transition relation: " << abstraction.getSize() << std::endl;
+
+
+
+  /****************************************************************************/
+  /* we continue with the controller synthesis */
+  /****************************************************************************/
+  // int verbose=1;
+  /* we setup a fixed point object to compute reachabilty controller */
+  scots::FixedPoint fp(&abstraction);
+  /* the fixed point algorithm operates on the BDD directly */
+  BDD T = ts.getSymbolicSet();
+
+  // Reach synthesis
+  // tt.tic();
+  // BDD C = fp.safe(T,1);
+  // BDD C = fp.pre(T);
+  // for(int i=1; i<=100; i++) {
+  //   BDD temp = fp.pre(C);
+  //   if(temp.IsZero()) {
+  //     std::cout << "Set is empty after " << i+1 << " iterations!!!" << std::endl;
+  //     break;
+  //   } else {
+  //     C = temp;
+  //   }
+  // }
+  // std::cout << "Empty: " << C.IsZero() << std::endl;
+  // C = fp.pre(C);
+  // std::cout << "Empty: " << C.IsZero() << std::endl;
+  // C = fp.pre(C);
+  // std::cout << "Empty: " << C.IsZero() << std::endl;
+  // // C = fp.pre(C);
+  // // std::cout << "Empty: " << C.IsZero() << std::endl;
+  // tt.toc();
+
+  // Reach and stay synthesis
+  tt.tic();
+  size_t i,j;
+  /* outer fp*/
+  BDD X=mgr.bddOne();
+  BDD XX=mgr.bddZero();
+  /* inner fp*/
+  BDD Y=mgr.bddZero();
+  BDD YY=mgr.bddOne();
+  /* the controller */
+  BDD C=mgr.bddZero();
+  BDD U=is.getCube();
+  /* as long as not converged */
+  for(i=1; XX != X; i++) {
+    X=XX;
+    BDD preX=fp.pre(X);
+    /* init inner fp */
+    YY = mgr.bddOne();
+    for(j=1; YY != Y; j++) {
+      Y=YY;
+      YY= ( fp.pre(Y) & T ) | preX;
+    }
+    XX=YY;
+    std::cout << "Iterations inner: " << j << std::endl;
+    /* remove all (state/input) pairs that have been added
+     * to the controller already in the previous iteration * */
+    BDD N = XX & (!(C.ExistAbstract(U)));
+    /* add the remaining pairs to the controller */
+    C=C | N;
+    //std::cout << C.CountMinterm(17) << std::endl;
+  }
+  std::cout << "Iterations outer: " << i << std::endl;
+  tt.toc();
+
+  /****************************************************************************/
+  /* last we store the controller as a SymbolicSet 
+   * the underlying uniform grid is given by the Cartesian product of 
+   * the uniform gird of the space and uniform gird of the input space */
+  /****************************************************************************/
+  scots::SymbolicSet controller(ss,is);
+  controller.setSymbolicSet(C);
+  std::cout << "Domain size: " << controller.getSize() << std::endl;
+  controller.writeToFile(controllerFile.c_str());
+
+  scots::SymbolicSet tr = abstraction.getTransitionRelation();
+  tr.writeToFile(transitionRelationFile.c_str());
+  
+  return 0;
+}
--- pendulate.m
+++ pendulate.m
@@ -26,11 +26,14 @@ close all
 A = 0.1; % Amplitude of vertical driving motion
 L = 1; % Length of pendulum
 g = 9.81; % Accleration of gravity
-gamma = 0.05; % Linear drag term
+gamma = 0; % Linear drag term
 
 % Precomputing constant terms
 alpha = g / L;
 beta = A / L;
+
+% Averaging constants
+avg_beta = 0.25 * beta^2;
 
 % ODE
 function dxdt = pendulate_ode(t,x,u)
@@ -41,46 +44,157 @@ function dxdt = pendulate_ode(t,x,u)
   dxdt(3)=u;
 end
 
+function dxdt = pendulate_averaged_ode(t,x,u)
+  dxdt = zeros(2,1);
 
-% target set
-% L=[2 0 0; 0 1 0; 0 0 .1];
-% c=[9.5; 0.6; 0];
+  dxdt(1)=x(2);
+  dxdt(2)=-alpha*sin(x(1)) - avg_beta*(u^2)*sin(2*x(1));
+end
 
-% initial state
-x0=[pi, -0.1, 0];
+function new_r = pendulate_growth(r,x,u,dt)
+    new_r = zeros(size(r));
 
-% controller=SymbolicSet('bdd/pendulate_controller.bdd','projection',[1 2 3]);
+    new_r(2) = r(2) + r(1)*abs(alpha*cos(x(1)) + 2*u*u*avg_beta*cos(2*x(1)))*dt;
+    % new_r(2) = r(2) + r(1)*abs(alpha + 2*u*u*avg_beta)*dt; % Conservative bound
+    new_r(1) = r(1) + new_r(2)*dt;
+end
 
-y=x0;
-v=[];
-time = [0];
-n_iter = 100;
+% Visualize growth bound
+% x0 = [pi,0.8];
+% bounds = [0.05, 0.05]/2;
+% dt = 0.1;
+% u = 50;
+% compute_growth(@pendulate_averaged_ode, @pendulate_growth, x0, bounds, dt, u)
+
+
+% Plot state, target, controller sets
+target_set = SymbolicSet('bdd/stay_pendulate_target.bdd');
+state_set = SymbolicSet('bdd/stay_pendulate_ss.bdd');
+controller = SymbolicSet('bdd/stay_pendulate_controller.bdd', 'projection',[1, 2]);
+% t_points = target_set.points;
+% s_points = state_set.points;
+% c_points = controller.points;
+% figure(1)
+% hold on;
+% scatter(s_points(:,1),s_points(:,2),'.','blue')
+% scatter(t_points(:,1)+.01,t_points(:,2)+.01,'.','red')
+% scatter(c_points(:,1)-.01,c_points(:,2)-.01,'.','green')
+% hold off;
+
+
+% Averaged ode
+x0=[2.8, -0.2];
+filename = 'images/driving_showcase.gif';
+
+n_iter = 150;
 dt = 0.1;
-u = 1;%8 + (1:n_iter)*dt*0.5;
+u = 50;
+y=x0;
+v=[1];
+time = [0];
 for i = 1:n_iter
 
-  
-  % if ( (y(end,:)-c')*L'*L*(y(end,:)'-c)<=1 )
-  %   break;
-  % end 
+    % Get input from controller
+    % try
+    %     u=min(controller.getInputs(y(end,:)));
+    % catch e
+    %     warning('Point not in controller!!!');
+    %     y(end,:)
+    %     break
+    % end
+    v=[v; u];
 
-  % u=controller.getInputs(y(end,:));
-  v=[v; u];
-  [t, x]=ode45(@pendulate_ode, [time(end), time(end) + dt], y(end,:),[],u);
+    % Integrate DE's
+    [t, x]=ode45(@pendulate_averaged_ode, [time(end), time(end) + dt], y(end,:),[],u);
 
-  time = [time; t(end)];
-  y=[y; x(end,:)];
+    % Record result
+    last_x = x(end,:);
+    last_x(1) = wrapTo2Pi(last_x(1));
+
+    time = [time; t(end)];
+    y=[y; last_x];
+
+    if i > n_iter/2
+        u = 1;
+    end
 end
 
-plot_trajectory(y, time, L)
+% y(end-10:end,:)
+
+plot_trajectory(y, time, v, target_set, false)
+
+if size(y,1) > n_iter
+    animate_trajectory(y, v, L, target_set, dt/2, filename)
+end
 
 end
 
-function plot_trajectory(x, t, L)
-    figure('Position', [100,100,1800,530])
-    tiledlayout(1,3, 'TileSpacing','tight')
+function compute_growth(ode, growth, x0, bounds, dt, u)
+    offsets = [1,1; 1,0; 1,-1; 0,1; 0,-1; -1,1; -1,0; -1,-1];
+    offset_x = x0 + (offsets.*bounds);
+    orig_area = prod(2*bounds)
+
+
+    [t, x]=ode45(ode, [0, dt], x0, [], u);
+    new_x = x(end,:);
+
+    % Integrate offset points
+    new_offset_x = zeros(size(offset_x));
+    for i = 1:8
+        [t, x]=ode45(ode, [0, dt], offset_x(i,:), [], u);
+        new_offset_x(i,:) = x(end,:);
+    end
+
+    % Convex hull
+    [hull_idx, hull_area] = convhull(new_offset_x);
+    hull_area
+
+    % Rectangular bounds from offsets
+    ub = max(new_offset_x);
+    lb = min(new_offset_x);
+    rect_area = (ub(1)-lb(1))*(ub(2)-lb(2))
+
+    % Formal growth bounds
+    corner_offsets = [1,1; 1,-1; -1,-1; -1,1];
+    new_bounds = growth(bounds, new_x, u, dt)
+    formal_area = prod(2*new_bounds)
+    formal_offset_x = new_x + (corner_offsets.*new_bounds);
+
+    hold on;
+    patch(formal_offset_x(:,1), formal_offset_x(:,2), [1,.6,.6])
+
+    % patch([lb(1),ub(1),ub(1),lb(1)], [lb(2),lb(2),ub(2),ub(2)], [.6,1,.6])
+
+    plot(new_offset_x(hull_idx,1), new_offset_x(hull_idx,2))
+
+    scatter(x0(1), x0(2), 25, 'blue')
+    scatter(offset_x(:,1), offset_x(:,2), 25, 'magenta')
+    scatter(new_x(1), new_x(2), 25, 'blue')
+    scatter(new_offset_x(:,1), new_offset_x(:,2), 25, 'red')
+    
+    xlabel('\theta')
+    ylabel('$\dot{\theta}$', 'Interpreter','latex')
+    title('Growth Bounds')
+    hold off;
+end
+
+function plot_trajectory(x, t, v, target_set, center_0)
+    if center_0
+        x(:,1) = wrapToPi(x(:,1));
+    end
+
+    figure('Position', [100,100,530,1130])
+    tiledlayout(2,1, 'TileSpacing','tight')
     
     ax1 = nexttile;
+    hold on;
+    lb1 = min(target_set.points(:,1:2),[],1)' - (target_set.eta(1:2) / 2);
+    ub1 = max(target_set.points(:,1:2),[],1)' + (target_set.eta(1:2) / 2);
+    patch([lb1(1),ub1(1),ub1(1),lb1(1)]/pi, [lb1(2),lb1(2),ub1(2),ub1(2)], [.6,1,.6])
+
+    % c_points = controller.points;
+    % scatter(c_points(:,1)/pi,c_points(:,2),'.','green')
+
     scatter(x(:,1)/pi, x(:,2), 25, '.', 'CData',t)
     title('Pendulum Phase Portrait')
     xlabel('\theta')
@@ -88,33 +202,100 @@ function plot_trajectory(x, t, L)
     colormap(ax1, winter)
     cb1 = colorbar;
     ylabel(cb1, 'Time (s)')
+    hold off;
 
     ax2 = nexttile;
-    scatter(t, x(:,1)/pi, 25, '.', 'CData',abs(x(:,2)))
+    hold on;
+    lb2 = [0, lb1(1)];
+    ub2 = [t(end), ub1(1)];
+    patch([lb2(1),ub2(1),ub2(1),lb2(1)], [lb2(2),lb2(2),ub2(2),ub2(2)]/pi, [.6,1,.6])
+    scatter(t, x(:,1)/pi, 25, '.', 'CData',v)
     title('Pendulum \theta vs Time')
     xlabel('Time')
     ylabel('\theta')
-    colormap(ax2, parula)
+    colormap(ax2, copper)
     cb2 = colorbar;
-    ylabel(cb2, 'Angular Speed ($|\dot{\theta}|$)', 'Interpreter','latex')
+    ylabel(cb2, 'Driving Angular Frequency')
+    hold off;
     
-    ax3 = nexttile;
-    scatter(0, 0, 50, 'red')
-    hold on
-    pos = [sin(x(:,1)), -cos(x(:,1))] * L;
-    % Color by w
-    % scatter(pos(:,1), pos(:,2), 25, '.', 'CData',abs(x(:,2)))
-    % colormap(ax3, parula)
+    % ax3 = nexttile;
+    % hold on;
+    % patch([0, 1.5*L*sin(lb1(1)), 1.5*L*sin(ub1(1))], ...
+    %       [0, -1.5*L*cos(lb1(1)), -1.5*L*cos(ub1(1))], [.6,1,.6])
+    % scatter(0, 0, 50, 'red')
+    % pos = [sin(x(:,1)), -cos(x(:,1))] * L;
+    % % Color by w
+    % % scatter(pos(:,1), pos(:,2), 25, '.', 'CData',abs(x(:,2)))
+    % % colormap(ax3, parula)
+    % % cb3 = colorbar;
+    % % ylabel(cb3, 'Angular Speed ($|\dot{\theta}|$)', 'Interpreter','latex')
+    % % Color by t
+    % scatter(pos(:,1), pos(:,2), 25, '.', 'CData',v)
+    % colormap(ax3, copper)
     % cb3 = colorbar;
-    % ylabel(cb3, 'Angular Speed ($|\dot{\theta}|$)', 'Interpreter','latex')
-    % Color by t
-    scatter(pos(:,1), pos(:,2), 25, '.', 'CData',t)
-    colormap(ax3, winter)
-    cb3 = colorbar;
-    ylabel(cb3, 'Time(s)')
+    % ylabel(cb3, 'Driving Angular Frequency')
+    % xlim([-L, L])
+    % ylim([-L, L])
+    % title('Pendulum Position')
+    % hold off;
+end
 
+function animate_trajectory(x, v, L, target_set, delay, filename)
+    fig = figure;
+
+    % Target patch
+    lb = min(target_set.points(:,1:2),[],1)' - (target_set.eta(1:2) / 2);
+    ub = max(target_set.points(:,1:2),[],1)' + (target_set.eta(1:2) / 2);
+    p = zeros(3,2);
+    p(:,1) = [0, 1.5*L*sin(lb(1)), 1.5*L*sin(ub(1))];
+    p(:,2) = [0, -1.5*L*cos(lb(1)), -1.5*L*cos(ub(1))];
+
+    % Animated trajectory
+    traj = zeros([2, size(x)]);
+    traj(2,:,1) = sin(x(:,1)) * L;
+    traj(2,:,2) = -cos(x(:,1)) * L;
+
+    % Driving freq visualization
+    cmap = copper(50);
+    colors = cmap(int8(v),:);
+
+    % Plot
+    hold on;
+    % patch(p(:,1), p(:,2), [.6,1,.6], 'EdgeColor',[.6,1,.6])
+    pivot = plot(0, 0, 'o', 'MarkerSize',20, 'MarkerFaceColor',colors(1,:), 'MarkerEdgeColor',colors(1,:));
+    pendulum = plot(traj(:,1,1), traj(:,1,2), 'LineWidth',5, 'Color','black');
     xlim([-L, L])
     ylim([-L, L])
-    title('Pendulum Position')
-    hold off
+    colormap(cmap)
+    cb3 = colorbar;
+    clim([1,50])
+    ylabel(cb3, 'Driving Angular Frequency')
+    axis off;
+    hold off;
+
+    if filename ~= ""
+        im = frame2im(getframe(fig));
+        [A,map] = rgb2ind(im,256);
+        imwrite(A, map, filename, 'gif', 'LoopCount',Inf, 'DelayTime',delay)
+    end
+
+    % Animate
+    for i = 1:size(x,1)
+        pause(delay)
+        pivot.MarkerFaceColor = colors(i,:);
+        pivot.MarkerEdgeColor = colors(i,:);
+        pendulum.XData = traj(:,i,1);
+        pendulum.YData = traj(:,i,2);
+        drawnow
+
+        if filename ~= ""
+            im = frame2im(getframe(fig));
+            [A,map] = rgb2ind(im,256);
+            imwrite(A, map, filename, 'gif', 'WriteMode','append', 'DelayTime',delay)
+        end
+    end
+
+    if filename ~= ""
+        fprintf("Animation saved to %s\n", filename)
+    end
 end
